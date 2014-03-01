ba392a6b0bda43d2bda9d71c57ac9ff4a2d468d6
JP Slavinsky <jpslav@gmail.com>
2014-02-28 18:06:41 -0800

    added roar utils, set version to 1.3.0

--- lib/openstax/utilities/development.rb
+++
@@ -1,8 +0,0 @@
-module OpenStax
-  module Utilities
-    module Development
-
-
-    end
-  end
-end
--- lib/openstax/utilities/version.rb
+++ lib/openstax/utilities/version.rb
@@ -1,5 +1,5 @@
 module OpenStax
   module Utilities
-    VERSION = "1.2.1.alpha"
+    VERSION = "1.3.0"
   end
 end
