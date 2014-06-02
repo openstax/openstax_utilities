c4871ea72a46146ab0ae0c5dfe07a974d88e5c24
Dante Soares <dms3@rice.edu>
2014-06-02 14:21:10 -0500

    generate_url now more useful as it concatenates urls

--- lib/openstax/utilities/network.rb
+++ lib/openstax/utilities/network.rb
@@ -1,7 +1,8 @@
 module OpenStax::Utilities
 
-  def self.generate_url(url, params = {})
-    uri = URI(url)
+  def self.generate_url(*urls)
+    params = urls.pop if urls.last.is_a? Hash
+    uri = URI.join(*urls)
     uri.query = params.to_query
     uri.to_s
   end
