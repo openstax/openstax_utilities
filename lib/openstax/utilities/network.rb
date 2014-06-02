7c802e88f803d7fdca83331adab7fed8d2326568
Dante Soares <dms3@rice.edu>
2014-06-02 14:24:39 -0500

    Allow nil params

--- lib/openstax/utilities/network.rb
+++ lib/openstax/utilities/network.rb
@@ -3,7 +3,7 @@ module OpenStax::Utilities
   def self.generate_url(*urls)
     params = urls.pop if urls.last.is_a? Hash
     uri = URI.join(*urls)
-    uri.query = params.to_query
+    uri.query = params.to_query if params
     uri.to_s
   end
 
